7e0dd43ea6aead8f516b3f0e401b4154e1993876
emerson-newart <emerson.art.n@hotmail.com>
2023-04-06 13:30:35 -0300

    Salvando o projeto

---
+++ src/main/java/br/com/emerson/AlugFacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.emerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugFacilApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/emerson/controller/PaginaInicialController.java
@@ -0,0 +1,18 @@
+package br.com.emerson.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class PaginaInicialController {
+
+	@GetMapping("home")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
+	}
+	
+}
