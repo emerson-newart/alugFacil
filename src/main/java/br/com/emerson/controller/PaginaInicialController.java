52882132b167e56c12552673fdf43da303d51d3b
emerson-newart <emerson.art.n@hotmail.com>
2023-04-06 13:40:26 -0300

    Criando o metodo teste

--- src/main/java/br/com/emerson/controller/PaginaInicialController.java
+++ src/main/java/br/com/emerson/controller/PaginaInicialController.java
@@ -3,6 +3,7 @@ package br.com.emerson.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,6 +14,13 @@ public class PaginaInicialController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("home");
 		return mv;
+	}
+	
+	@GetMapping("/teste")
+	@ResponseBody
+	public String teste() {
+		String nome = "Emerson";
+		return nome;
 	}
 	
 }
